caeea44e50662ff190bc266d75af426563138043
Giovane Boaviagem <giovanebribeiro@gmail.com>
2017-06-27 21:33:40 -0300

    Add alias to reload bashrc and add remote branch attatched to current branch in PS1

--- terminal/aliases_Darwin.sh
+++ terminal/aliases_Darwin.sh
@@ -4,3 +4,4 @@ alias ps="ps aux"
 alias ls='ls -G'
 alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
 alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
+alias xsel="pbcopy"
