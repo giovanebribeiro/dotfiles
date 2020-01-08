936313135df3c56f8bf3c0a97ff901ffcfacd6cb
Giovane Boaviagem <giovanebribeiro@gmail.com>
2020-01-08 15:56:58 -0300

    add new vim markdown plugin

--- ide/main.sh
+++ ide/main.sh
@@ -70,7 +70,11 @@ else
 fi
 
 printSubsection "Markdown"
-$INSTALL markdown
+if [ ! -d $HOME/bin ]; then
+    mkdir $HOME/bin
+fi
+wget -O $HOME/bin/mdr https://github.com/MichaelMure/mdr/releases/download/v0.2.2/mdr_linux_amd64
+chmod +x $HOME/bin/mdr
 
 # Git configurations
 printSubsection "Git configurations"
