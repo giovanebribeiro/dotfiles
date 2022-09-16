bdc8f9444d5a84a9dcf3dc1df3899e6b66a89c7a
Giovane Boaviagem <giovanebribeiro@gmail.com>
2022-09-16 00:39:57 -0300

    feat: add terminal module

--- terminal/module.sh
+++ terminal/module.sh
@@ -1,62 +1,60 @@
 source $PWD/__setup/util.sh
-OS=`uname`
 
+OS=`cat $CMD_FILE`
 
-pre_ubuntu(){
-    printSection "BASIC INSTALLATIONS"
+_pre_arch(){
+    printSection "instala os pacotes necessários para a distro arch linux, módulo terminal"
 
+    sudo pacman -Sy zsh alacritty powerline
+
+    printOK
     
 }
 
 install(){
 
 
-    # installing zsh
-    installPkg "zsh"
+	if [ ! -f $DOT_FOLDER/terminal.lock ]; then
 
-    if [ "$SHELL" -ne "/usr/bin/zsh" ]; then
-        sudo chsh -s /usr/bin/zsh $USER
-    fi
+		if [ ! -f $DOT_FOLDER/base.lock_module ]; then
+			_pre_$OS 
+			touch $DOT_FOLDER/base.lock_module
+		fi
+		
+		curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+
+		printSubsection "Installing typewritten theme for zsh"
+		git clone https://github.com/reobin/typewritten.git $ZSH/themes/typewritten
+		ln -s "$ZSH/themes/typewritten/typewritten.zsh-theme" "$ZSH/themes/typewritten.zsh-theme"
     
-    if [ ! -d $HOME/.oh-my-zsh ]; then
-	    echo "You not install oh-my-zsh. Please install it:"
-	    echo '$ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended"'
-	    exit 0
-    fi
-    
-    # We must do this again because oh-my-zsh overwrites our zshrc.
-    if [ -f "$HOME/.zshrc" ]; then
-        rm $HOME/.zshrc
-    fi
+		stowit $PWD/base
 
-    stowit $PWD/base/base
-    rm $HOME/bin/change_volume
-    ln -s $PWD/base/change_volume.sh $HOME/bin/change_volume
-    source $HOME/.zshrc
+		printOK
 
-    if [ ! -d "$ZSH_CUSTOM/themes/typewritten" ]; then
-        printSubsection "Installing typewritten theme for zsh"
-        git clone https://github.com/reobin/typewritten.git $ZSH/themes/typewritten
-        ln -s "$ZSH/themes/typewritten/typewritten.zsh-theme" "$ZSH/themes/typewritten.zsh-theme"
-    fi
+	fi
 
-    printSubsection "Installing aliases..."
-    if [ "$OS" = "Darwin" ]; then
-        aliasit "ls" "'ls -G'"
-        aliasit "showFiles" "'defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'"
-        aliasit "hideFiles" "'defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'"
-    elif [ "$OS" = "Linux" ]; then
-        aliasit "ls" "'ls --color=auto'"
-    fi
 
-    printSubsection "Installing exports..."
+    #rm $HOME/bin/change_volume
+    #ln -s $PWD/base/change_volume.sh $HOME/bin/change_volume
+    #source $HOME/.zshrc
 
-    if [ "$OS" = "Darwin" ]; then
-        exportit "CLICOLOR" "'true'"
-        exportit "LSCOLORS" "gxfxcxdxbxCgCdabagacad"
-    fi
+    #printSubsection "Installing aliases..."
+    #if [ "$OS" = "Darwin" ]; then
+    #    aliasit "ls" "'ls -G'"
+    #    aliasit "showFiles" "'defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'"
+    #    aliasit "hideFiles" "'defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'"
+    #elif [ "$OS" = "Linux" ]; then
+    #    aliasit "ls" "'ls --color=auto'"
+    #fi
 
-    printOK
+    #printSubsection "Installing exports..."
+
+    #if [ "$OS" = "Darwin" ]; then
+    #    exportit "CLICOLOR" "'true'"
+    #    exportit "LSCOLORS" "gxfxcxdxbxCgCdabagacad"
+    #fi
+
+    #printOK
     
 }
 
