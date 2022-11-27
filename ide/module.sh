b316a66d55141cf8a39e638de62f2fc6dd3e0643
Giovane Boaviagem Ribeiro <giovanebribeiro@gmail.com>
2022-11-26 23:28:55 -0300

    install ide module

--- ide/module.sh
+++ ide/module.sh
@@ -24,19 +24,29 @@ _pre_arch(){
 
 _pre_ubuntu(){
 
-	printSubsection "instala os pacotes necessários para a distro arch linux, módulo ide"
+	printSubsection "instala os pacotes necessários para a distro ubuntu, módulo ide"
 
 	# meld é uma aplicação estilo winmerge
-	sudo pacman -Sy vim-gtk docker exuberant-ctags meld
-
-	bkp=$PWD
+	sudo apt install software-properties-common apt-transport-https wget vim-gtk exuberant-ctags meld -y
 
 	# instalando vscode
-	printSubSubsection "Installing VSCode"
-	git clone https://aur.archlinux.org/visual-studio-code-bin.git $HOME/.aur/visual-studio-code-bin
-       	cd $HOME/.aur/visual-studio-code-bin
-	makepkg -si --needed	
-	cd $bkp
+	#printSubSubsection "Installing VSCode"
+	#wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+	#sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+	#sudo apt-get update
+	#sudo apt install code
+
+	#printSubSubsection "Installing VSCode"
+	#sudo apt-get install ca-certificates curl gnupg lsb-release
+	#sudo mkdir -p /etc/apt/keyrings
+	#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+	#echo \
+	#  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+	#    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+	#sudo apt-get update
+	#sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+	#sudo groupadd docker
+	#sudo usermod -aG docker $USER
 
 	printOK
 
@@ -57,7 +67,7 @@ install(){
 	printSubsection "VIM configurations"
 
 	# copiando .vimrc
-	stowit $PWD/base
+	stowit $PWD/ide/base
 
 	if [ ! -d $HOME/.vim/bundle ]; then
 		printSubSubsection "Installing VIM plugins"
@@ -81,28 +91,28 @@ install(){
 	git config --global core.excludesfile '~/.gitignore_global'
 	git config --global init.templatedir '~/.git-templates'
 
-	printSubsection "NodeJS configurations"
-	if [ ! -f $HOME/.npmrc ]; then
-		printSubSubsection "Loading npm informations"
-		npm login
-	fi
+	#printSubsection "NodeJS configurations"
+	#if [ ! -f $HOME/.npmrc ]; then
+	#	printSubSubsection "Loading npm informations"
+	#	npm login
+	#fi
 
-	# npm-check-updates
-	installNodePackage "npm-check-updates"
+	## npm-check-updates
+	#installNodePackage "npm-check-updates"
 
-	printSubsection "Markdown support and other useful tools"
-	# instalando mdr (interpertador markdown)
-	mkdir -p $HOME/bin 2>/dev/null
-	wget -O $HOME/bin/mdr https://github.com/MichaelMure/mdr/releases/download/v0.2.5/mdr_linux_amd64
-	chmod +x $HOME/bin/mdr
+	#printSubsection "Markdown support and other useful tools"
+	## instalando mdr (interpertador markdown)
+	#mkdir -p $HOME/bin 2>/dev/null
+	#wget -O $HOME/bin/mdr https://github.com/MichaelMure/mdr/releases/download/v0.2.5/mdr_linux_amd64
+	#chmod +x $HOME/bin/mdr
 
-	if [ ! -f $HOME/.cargo/bin/what ]; then
-		printSubSubsection "Install what (network packet sniffer)"
-		cargo install what
-		sudo ln -s $HOME/.cargo/bin/what /usr/local/bin/what
-	else
-		printSubSubsection "what already installed"
-	fi
+	#if [ ! -f $HOME/.cargo/bin/what ]; then
+	#	printSubSubsection "Install what (network packet sniffer)"
+	#	cargo install what
+	#	sudo ln -s $HOME/.cargo/bin/what /usr/local/bin/what
+	#else
+	#	printSubSubsection "what already installed"
+	#fi
 
 	touch $DOT_FOLDER/ide.lock
 	printOK
